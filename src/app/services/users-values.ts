0df7e52a25343d95c17b3624d634b5016c7d9a69
Fernando Saibene <fsaibene@axoft.com>
2021-04-24 09:45:59 -0300

    usuarios predefinidos

---
+++ src/app/services/users-values.ts
@@ -0,0 +1,6 @@
+export const PredefinedUsers = [
+    {"id":1, "correo":"admin@admin.com", "clave":1111, "perfil":"admin", "sexo":"femenino"},
+    {"id":2, "correo":"invitado@invitado.com", "clave":2222, "perfil":"invitado", "sexo":"femenino"},
+    {"id":3, "correo":"usuario@usuario.com", "clave":3333, "perfil":"usuario", "sexo":"masculino"},
+    {"id":4, "correo":"anonimo@anonimo.com", "clave":4444, "perfil":"usuario", "sexo":"masculino"},
+    {"id":5, "correo":"tester@tester.com", "clave":5555, "perfil":"tester","sexo": "femenino"}];
